f1f6311f4293c813dc2b475d9482f7dfc103556a
German Om <germanom91@gmail.com>
2017-02-09 19:09:38 -0800

    Initial commit of existing files.

---
+++ PokerHandEvaluator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PokerHandEvaluator
+//
+//  Created by German Om on 2/9/17.
+//  Copyright © 2017 German Om. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
