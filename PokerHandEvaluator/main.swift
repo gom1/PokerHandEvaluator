c5d31290a6a6103bf62ff9246ddeab93b415d8a9
German Om <germanom91@gmail.com>
2017-02-11 18:53:49 -0800

    Created a PokerHand Struct and added tests for valid Poker Hands

--- PokerHandEvaluator/main.swift
+++ PokerHandEvaluator/main.swift
@@ -6,7 +6,80 @@
 //  Copyright © 2017 German Om. All rights reserved.
 //
 
-import Foundation
+enum PokerSuit: String {
+    case hearts
+    case diamonds
+    case clubs
+    case spades
+}
 
-print("Hello, World!")
+enum PokerValue: Int{
+    case ace = 1, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
+    }
+
+struct Card {
+    let value: PokerValue
+    let suit: PokerSuit
+    init(_ value: PokerValue, _ suit: PokerSuit) {
+        self.value = value
+        self.suit = suit
+    }
+}
+
+enum PokerHandRank: String{
+    case HighCard
+    case OnePair
+    case TwoPairs
+    case ThreeOfAKind
+    case Straight
+    case Flush
+    case FullHouse
+    case FourOfAKind
+    case StraightFlush
+}
+
+struct PokerHand {
+    let card1: Card
+    let card2: Card
+    let card3: Card
+    let card4: Card
+    let card5: Card
+    
+    let hand: [Card]
+    
+    init (_ cards: [Card]) {
+        self.card1 = cards[0]
+        self.card2 = cards[1]
+        self.card3 = cards[2]
+        self.card4 = cards[3]
+        self.card5 = cards[4]
+        self.hand = [card1, card2, card3, card4, card5]
+    }
+    
+    func isHandValid() -> Bool {
+        for i in 0...hand.count - 2 {
+            for j in i+1...hand.count - 1 {
+                print (hand[i].value, hand[i].suit, hand[j].value, hand[j].suit)
+                print (i, j)
+                if (hand[i].value, hand[i].suit) == (hand[j].value, hand[j].suit) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
+
+//class PokerHandEvaluator {
+//    let cards =
+//}
+
+let card1 = Card(PokerValue.king, PokerSuit.hearts)
+let card2 = Card(PokerValue.king, PokerSuit.diamonds)
+let card3 = Card(PokerValue.king, PokerSuit.diamonds)
+let card4 = Card(PokerValue.three, PokerSuit.spades)
+let card5 = Card(PokerValue.four, PokerSuit.spades)
+let pokerHand = PokerHand([card1, card2, card3, card4, card5])
+
+print (pokerHand.isHandValid())
 
--- PokerHandEvaluatorTests/PokerHandEvaluatorTests.swift
+++ PokerHandEvaluatorTests/PokerHandEvaluatorTests.swift
@@ -7,27 +7,45 @@
 //
 
 import XCTest
+@testable import PokerHandEvaluator
 
 class PokerHandEvaluatorTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
+    func testAreCardsValid() {
+        let card = Card(PokerValue.king, PokerSuit.hearts)
+        XCTAssertEqual(card.value, PokerValue.king)
+        XCTAssertEqual(card.suit, PokerSuit.hearts)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testValidPokerHand() {
+        let card1 = Card(PokerValue.king, PokerSuit.hearts)
+        let card2 = Card(PokerValue.jack, PokerSuit.hearts)
+        let card3 = Card(PokerValue.ten, PokerSuit.diamonds)
+        let card4 = Card(PokerValue.three, PokerSuit.spades)
+        let card5 = Card(PokerValue.four, PokerSuit.spades)
+        let pokerHand = PokerHand([card1, card2, card3, card4, card5])
+        XCTAssertTrue(pokerHand.isHandValid())
     }
     
+    func testInvalidPokerHand() {
+        // Hand with Repeated Cards
+        let card1 = Card(PokerValue.king, PokerSuit.hearts)
+        let card2 = Card(PokerValue.king, PokerSuit.hearts)
+        let card3 = Card(PokerValue.king, PokerSuit.hearts)
+        let card4 = Card(PokerValue.king, PokerSuit.hearts)
+        let card5 = Card(PokerValue.king, PokerSuit.hearts)
+        
+        let pokerHand = PokerHand([card1, card2, card3, card4, card5])
+        XCTAssertFalse(pokerHand.isHandValid())
+    }
+
 }
+
